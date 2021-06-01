d1a75bd45015dfb41901010564134e31c675714d
Zining Yu <grace.yu@meadowridge.bc.ca>
2021-06-01 12:17:15 -0700

    update

--- Action_Potentials_Website_Self/calendar.py
+++ Action_Potentials_Website_Self/calendar.py
@@ -4,3 +4,7 @@ import calendar
 c = calendar.TextCalendar(calendar.SUNDAY)
 str = c.formatmonth(2021,5)
 print(str)
+
+d = calendar.TextCalendar(calendar.SUNDAY)
+strd = d.formatmonth(2021,7)
+print(strd)
